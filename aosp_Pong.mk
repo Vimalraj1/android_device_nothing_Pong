5872fd0c11453053d3425cb062ba9f32d9a06fd0
Vimalraj1 <vimalrajbalaji@gmail.com>
2024-05-29 11:56:36 +0530

    Pong: Parasitizing

--- aosp_Pong.mk
+++ aosp_Pong.mk
@@ -11,10 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from Pong device
 $(call inherit-product, device/nothing/Pong/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common TheParasiteProject stuffs.
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_INCLUDE_CAMERA_GO := true
+TARGET_SUPPORTS_LILY_EXPERIENCE := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := false
+MAINLINE_INCLUDE_VIRT_MODULE := false
+TARGET_GBOARD_KEY_HEIGHT := 1.1
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_Pong
+PRODUCT_NAME := aosp_Pong
 PRODUCT_DEVICE := Pong
 PRODUCT_MANUFACTURER := nothing
 PRODUCT_BRAND := Nothing
@@ -25,7 +36,7 @@ PRODUCT_SYSTEM_DEVICE := Pong
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-PRODUCT_GMS_CLIENTID_BASE := android-nothing
+PRODUCT_GMS_CLIENTID_BASE := android-google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="Pong-user 12 SKQ1.230722.001 2404101247 release-keys" \
