b0c0cfa5eb43c0393f1ff37785dfc169023c38a6
someone5678 <someone5678@users.noreply.github.com>
2024-05-30 12:27:36 +0530

    Pong: Migrate to common libqti-perfd-client and power-libperfmgr
    Change-Id: I22dd0baaf1594db1650bccf37b4d167b50135174

--- device.mk
+++ device.mk
@@ -386,11 +386,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    android.hardware.power@1.2.vendor \
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # PowerShare
 PRODUCT_PACKAGES += \
@@ -443,7 +441,11 @@ PRODUCT_SHIPPING_API_LEVEL := 32
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
