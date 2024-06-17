ec944a02df9e4d16dd0109d9da1a3c26928af7d2
Vimalraj1 <vimalrajbalaji@gmail.com>
2024-06-17 10:46:49 +0530

    Pong: Alpha Bringup

--- lineage_Pong.mk
+++ lineage_Pong.mk
@@ -11,8 +11,40 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from Pong device
 $(call inherit-product, device/nothing/Pong/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Alpha stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# TARGET_LAUNCHER options:
+# 1 - stock (default)
+# 2 - lawnchair
+# 3 - pixel (valid only on gapps builds)
+TARGET_LAUNCHER := 3
+
+# GAPPS (valid only for GAPPS builds)
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+
+# Maintainer
+#ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := The_Unluck
 
 PRODUCT_NAME := lineage_Pong
 PRODUCT_DEVICE := Pong
